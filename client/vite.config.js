909ba20df2275396e0c165a68a6630fc1a8574dd
Mayank Jain <mayankjainx404@gmail.com>
2023-04-23 02:19:38 +0530

    Changed vite.config to build in server

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react()],
-  //  server: {
-  //     proxy: {
-  //        "/api": {
-  //           target: "http://localhost:4000",
-  //           changeOrigin: true,
-  //           secure: false,
-  //        },
-  //     },
-  //  },
+   build: {
+      outDir: "../server/dist",
+   },
 });
