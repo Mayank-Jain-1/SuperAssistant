70a562296f4af74077839a7a0893c4f68b382701
Mayank Jain <mayankjainx404@gmail.com>
2023-04-22 18:06:24 +0530

    Tailwind init in the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeSearch from "./pages/HomeSearch";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<HomeSearch />} />
+         </Routes>
+      </BrowserRouter>
+   );
+};
 
-export default App
+export default App;
---
+++ client/src/pages/HomeSearch.jsx
@@ -0,0 +1,11 @@
+
+const HomeSearch = () => {
+  return (
+    <div>
+      <h1 className="text-5xl">Super Assisstant</h1>
+      <iframe width="1048" height="550" src="https://www.youtube.com/embed/oL1uem6-3m4" title="#1 ServiceNow Development Training - Users, Group and Role | ServiceNow Training Online" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+  )
+}
+
+export default HomeSearch
